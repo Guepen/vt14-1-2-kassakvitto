ac0e29a0ea8a6d4fd84ae9ed6b55606e58ba9eed
th222fa <th222fa@student.lnu.se>
2014-01-28 11:40:07 +0100

    utskrift kvar och design kvar

--- Kassakvitto/Kassakvitto/Default.aspx.cs
+++ Kassakvitto/Kassakvitto/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Kassakvitto.Model;
 
 namespace Kassakvitto
 {
@@ -16,7 +17,11 @@ namespace Kassakvitto
 
         protected void Send_Click(object sender, EventArgs e)
         {
+            if (IsValid)
+            {
+                var receipt = new Receipt(double.Parse(Input.Text)); 
 
+            }
         }
     }
 }
